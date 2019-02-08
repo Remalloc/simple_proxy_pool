68b95cf2949e8eedf5785cc0454e8d82e1c557b5
linzeen <remalloc.virtual@gmail.com>
2019-02-08 11:11:54 +0800

    [modify] setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,12 @@ setup(
     author="remalloc",
     author_email="remalloc.virtual@gmail.com",
     url="https://github.com/Remalloc/simple_proxy_pool",
-    packages=["simple_proxy_pool", "unit_test"]
+    packages=["simple_proxy_pool", "unit_test"],
+    install_requires=[
+        "aiohttp>=3.5.4",
+        "beautifulsoup4>=4.7.1",
+        "lxml>=4.3.0"
+    ],
+    python_requires=">=3.6",
+    license='BSD'
 )
